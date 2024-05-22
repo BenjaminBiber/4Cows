3ed6237971ae154b4902047b04d4ec5924c170b8
Benjamin Biber <benjamin.biber@4sellers.de>
2024-05-22 12:45:44 +0200

    Added IIS_Updater

---
+++ IIS_Updater/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IIS_Updater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
