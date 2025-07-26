63d0468e4e80691b04f8c1174097502b74c36471
Benjamin Biber (Linux) <ben.biber.strass@gmail.com>
2025-07-26 19:10:19 +0200

    Added WebScraperJob service and updated middleware configuration

--- XLinkScraper/Program.cs
+++ XLinkScraper/Program.cs
@@ -13,13 +13,14 @@ builder.Services.AddEndpointsApiExplorer();
 LoggerService.InitializeLogger();
 builder.Services.AddSingleton<CowService>();
 builder.Services.AddSingleton<XLinkService>();
+builder.Services.AddTransient<WebScraperJob>();
 builder.Services.AddSwaggerGen();
 builder.Configuration.AddEnvironmentVariables();
 DatabaseService.GetDBStringFromEnvironment();
 Console.WriteLine(Environment.GetEnvironmentVariable("REDIS_URL"));
-builder.Services.AddHangfire(config =>
-    config.UseRedisStorage(Environment.GetEnvironmentVariable("REDIS_URL") ?? "192.168.50.225:6379")); 
-builder.Services.AddHangfireServer();
+//builder.Services.AddHangfire(config =>
+    //config.UseRedisStorage(Environment.GetEnvironmentVariable("REDIS_URL") ?? "192.168.50.225:6379")); 
+//builder.Services.AddHangfireServer();
 var app = builder.Build();
 
 var dashboardOptions = new DashboardOptions
@@ -27,7 +28,7 @@ var dashboardOptions = new DashboardOptions
     Authorization = new[] { new CustomAuthorizationFilter() }
 };
 
-app.UseHangfireDashboard("", dashboardOptions);
+//app.UseHangfireDashboard("", dashboardOptions);
 
 if (app.Environment.IsDevelopment())
 {
@@ -36,8 +37,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseAuthorization();
+app.UseRouting(); // Falls noch nicht vorhanden
+app.UseAuthorization(); // schon vorhanden
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.MapControllers();
-RecurringJob.AddOrUpdate<WebScraperJob>("WebScraperJob", x => x.ExecuteAsync(), Cron.Daily);
+//BackgroundJob.Enqueue(() => Console.WriteLine("Testjob läuft!"));
+//RecurringJob.AddOrUpdate<WebScraperJob>("WebScraperJob", x => x.ExecuteAsync(), Cron.Daily);
 app.Run();
